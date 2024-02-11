381e2f5ced33a3b32325fdfdde1e49e8b2628091
tomhosker <tomdothosker@gmail.com>
2024-02-11 22:19:10 +0000

    Completed first test module.

--- tests/test_almanack_utils.py
+++ tests/test_almanack_utils.py
@@ -3,7 +3,7 @@ This code tests the functions in the almanack_utils module.
 """
 
 # Source imports.
-from source.almanack_utils import fetch_to_dict
+from source.almanack_utils import fetch_to_dict, get_loadout
 
 ###########
 # TESTING #
@@ -15,3 +15,7 @@ def test_fetch_to_dict():
     extract = fetch_to_dict(select, (1,))
     expected_opening = "Thou still unravished bride of quietness"
     assert extract[0]["content"].startswith(expected_opening)
+
+def test_get_loadout():
+    """ Test that the function runs. """
+    assert isinstance(get_loadout("main"), str)
